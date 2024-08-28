38e6a9da511597c476ef9a2a11dbecaaa132135e
Nrapendra Trivedi <trivajay259@gmail.com>
2024-08-29 00:36:58 +0200

    commit message

---
+++ my-demo-ui/src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styles: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
